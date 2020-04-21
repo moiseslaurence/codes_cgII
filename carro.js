b3788ddd2ce0d4034edec6d4420d0bb89306c920
Moises Laurence <moises.laurence@gmail.com>
2020-04-20 22:39:56 -0300

    Códigos das ultimas aulas

---
+++ carro.js
@@ -0,0 +1,11 @@
+function Carro(cor,velocMaxima){
+    this.cor = cor;
+    this.velocMaxima = velocMaxima;
+    this.velocAtual = 0;
+}
+
+Carro.prototype = {
+    acelerar: function(){
+        this.velocAtual += 10;
+    }
+}
